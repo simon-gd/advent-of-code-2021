6c6e9561b2b341553e7fe254f65824b5a522f41f
simon-gd <57882634+simon-gd@users.noreply.github.com>
2021-12-10 09:36:44 -0500

    adding day 9 and 10

---
+++ day10.js
@@ -0,0 +1,121 @@
+const fs = require('fs');
+// Load in a text file
+let text = fs.readFileSync('day10.txt', 'utf8');
+// Split the text into an array of strings
+let lines = text.split('\r\n');
+//console.log(lines);
+let illigalChars = [];
+function isOpen(char) {
+    if (char === '(' || char === '[' || char === '{' || char === '<') {
+        return true;
+    }
+    return false;
+}
+function isClose(char) {
+    if (char === ')' || char === ']' || char === '}' || char === '>') {
+        return true;
+    }
+    return false;
+}
+function isMatching (open, close) {
+    if (open === '(' && close === ')') {
+        return true;
+    }
+    else if (open === '[' && close === ']') {
+        return true;
+    } else if(open === '{' && close === '}') {
+        return true;
+    } else if(open === '<' && close === '>') {
+        return true;
+    }
+    return false;
+}
+for(let line of lines){
+    let queue = [];
+    for(let char of line){
+        if(isOpen(char)){
+            queue.push(char);
+        } else {
+            // is close char
+            if(queue.length === 0){
+                console.error('queue.length === 0', char);
+                illigalChars.push(char);
+                break;
+            } else {
+                let open = queue.pop();
+                if(!isMatching(open, char)){
+                    illigalChars.push(char);
+                    break;
+                }
+            }
+        }
+    }
+}
+// calculate score
+let score = 0;
+for(let char of illigalChars){
+    if(char === ')'){
+        score += 3;
+    } else if(char === ']'){
+        score += 57;
+    }else if(char === '}'){
+        score += 1197;
+    }else if(char === '>'){
+        score += 25137;
+    }
+}
+console.log('score', score);
+
+// Part 2
+function getMatchingChar(open){
+    if(open === '('){
+        return ')';
+    } else if(open === '['){
+        return ']';
+    } else if(open === '{'){
+        return '}';
+    } else if(open === '<'){
+        return '>';
+    }
+}
+
+let scores = [];
+for(let line of lines){
+    let queue = [];
+    let valid = true;
+    for(let char of line){
+        if(isOpen(char)){
+            queue.push(char);
+        } else {
+            let open = queue.pop();
+            if(!isMatching(open, char)){
+                valid = false;
+                break;
+            }
+        }
+    }
+    if(valid){
+        let score = 0;
+        let completionStrings = [];
+        while(queue.length > 0){
+            let open = queue.pop();
+            let matchingChar = getMatchingChar(open);
+            completionStrings.push(matchingChar);
+        }
+        for(let char of completionStrings){
+            score *= 5;
+            if(char === ')'){
+                score += 1;
+            } else if(char === ']'){
+                score += 2;
+            }else if(char === '}'){
+                score += 3;
+            }else if(char === '>'){
+                score += 4;
+            }
+        }
+        scores.push(score);
+    }
+}
+scores = scores.sort((a, b) => { return a - b; });
+console.log('score part 2', scores[(scores.length-1)  / 2]); 
---
+++ day9.js
@@ -0,0 +1,98 @@
+const fs = require('fs');
+// Load in a text file
+let text = fs.readFileSync('day9.txt', 'utf8');
+// Split the text into an array of strings
+let lines = text.split('\r\n');
+let width = lines[0].length;
+let height = lines.length;
+let grid = lines.map(line => line.split('').map(c => Number(c)));
+console.log(width, height);
+// Part 1
+function getNeighbors(x, y) {
+    let neighbors = [];
+    let dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+    for(let d of dirs) {
+        let nx = x + d[0];
+        let ny = y + d[1];
+        if (nx < 0 || nx >= width || ny < 0 || ny >= height) continue;
+        neighbors.push(grid[ny][nx]);
+    }
+    return neighbors;
+
+}
+let localMins = [];
+for(let x=0; x < width; x++) {
+  for(let y=0; y < height; y++) {
+    let neighbors = getNeighbors(x, y);
+    let minNeighbor = Math.min(...neighbors);
+    if(grid[y][x] < minNeighbor) {
+        localMins.push(grid[y][x]);
+    }
+  }
+}
+let totalRisk  = localMins.map(n => n+1).reduce((a,b) => a+b);
+console.log('totalRisk', totalRisk);
+
+// Part 2
+
+function getNeighborPos(x, y) {
+    let neighbors = [];
+    let dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+    for(let d of dirs) {
+        let nx = x + d[0];
+        let ny = y + d[1];
+        if (nx < 0 || nx >= width || ny < 0 || ny >= height) continue;
+        if(grid[ny][nx] < 9) {
+            neighbors.push([nx, ny]);
+        }
+    }
+    return neighbors;
+}
+
+function floodFill(x, y) {
+    //console.log('visited' , visited);
+    let queue = [[x, y]];
+    let visited = {};
+    while(queue.length > 0) {
+        
+        let [x1, y1] = queue.shift();
+        //console.log('queue', queue.length);
+        
+        if(grid[y1][x1] < 9) {
+            if (!visited[`${x1},${y1}`]) {
+                visited[`${x1},${y1}`] = true;
+            }
+            
+            let neighbors = getNeighborPos(x1, y1);
+            for(let n of neighbors) {
+                if(!visited[`${n[0]},${n[1]}`]) {
+                    queue.push(n);
+                    visited[`${n[0]},${n[1]}`] = true;
+                }
+            }
+        }
+    }
+    return visited;
+}
+
+let minPts = [];
+for(let x=0; x < width; x++) {
+  for(let y=0; y < height; y++) {
+    let neighbors = getNeighbors(x, y);
+    let minNeighbor = Math.min(...neighbors);
+    if(grid[y][x] < minNeighbor) {
+        minPts.push([x, y])
+    }
+  }
+}
+let basinCount = []
+for(let pt of minPts) {
+    
+    //console.log(pt[0], pt[1]);
+    let visited = floodFill(pt[0], pt[1]);
+    basinCount.push(Object.keys(visited).length);
+    //console.log('visited', pt[0], pt[1], Object.keys(visited).length);
+    
+}
+basinCount.sort((a,b) => b-a);
+console.log('basinCount', basinCount[0] *basinCount[1] * basinCount[2], basinCount);
