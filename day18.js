17be3c8667e2d1e882869a142c9f733c9c476481
simon-gd <57882634+simon-gd@users.noreply.github.com>
2021-12-18 13:58:44 -0500

    a bit of cleanup

--- day18.js
+++ day18.js
@@ -55,26 +55,9 @@ function convertToSnailNumber(input, type){
     }
 }
 
-function flatten(input) {
-    const stack = [...input];
-    const res = [];
-    while (stack.length) {
-      // pop value from stack
-      const next = stack.pop();
-      if (Array.isArray(next)) {
-        // push back array items, won't modify the original input
-        stack.push(...next);
-      } else {
-        res.push(next);
-      }
-    }
-    // reverse to restore input order
-    return res.reverse();
-  }
-
 function flattenTree(tree){
     let snailNumberToArray = SnailNumberToArray(tree);
-    return flatten(snailNumberToArray);
+    return snailNumberToArray.flat(Infinity);
 }
 
 function getNeighborNumber(flattenedTree, id, direction){
